b80a892f01eb6afd15272be39f110b2642a0ba0e
JoshuaMills <Jbmills291@gmail.com>
2018-03-10 13:56:02 -0500

    add code logic

--- app/src/main/java/com/mills/b/joshua/calculator/MainActivity.java
+++ app/src/main/java/com/mills/b/joshua/calculator/MainActivity.java
@@ -2,12 +2,105 @@ package com.mills.b.joshua.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Array;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+    private int[] numberButtonIDList = {
+            R.id.button0, R.id.button1, R.id.button2,
+            R.id.button3, R.id.button4, R.id.button5,
+            R.id.button6, R.id.button7, R.id.button8,
+            R.id.button9, R.id.buttonDot,
+    };
+    private int[] operationButtonIDList = {
+            R.id.buttonEquals, R.id.buttonDivide, R.id.buttonMultiply,
+            R.id.buttonMinus, R.id.buttonPlus
+    };
+
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayOperation = findViewById(R.id.operation);
+        result = findViewById(R.id.result);
+        newNumber = findViewById(R.id.newNumber);
+        setNumberButtonListener();
+        setOperationButtonListener();
+    }
+
+    private void setOperationButtonListener(){
+        for (int id:operationButtonIDList) {
+            findViewById(id).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String op = ((Button) v).getText().toString();
+                    String value = newNumber.getText().toString();
+                    if(value.length() != 0){
+                        performOperation(value,op);
+                    }
+                    pendingOperation = op;
+                    displayOperation.setText(pendingOperation);
+                }
+            });
+        }
+    }
+
+    private void setNumberButtonListener(){
+        for (int id:numberButtonIDList) {
+            findViewById(id).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    newNumber.append(((Button)v).getText().toString());
+                }
+            });
+        }
+
+    }
+
+    private void performOperation(String value,String op){
+        if(operand1 == null){
+            operand1 = Double.valueOf(value);
+        }else{
+            operand2 = Double.valueOf(value);
+            if(pendingOperation.equals("=")){
+                pendingOperation = op;
+            }
+            switch (pendingOperation){
+                case "=":
+                    operand1 = operand2;
+                    break;
+                case "/":
+                    if(operand2 == 0){
+                        operand1 = 0.0;
+                    }else{
+                        operand1 /= operand2;
+                    }
+                    break;
+                case "*":
+                    operand1 *= operand2;
+                    break;
+                case "-":
+                    operand1 -= operand2;
+                    break;
+                case "+":
+                    operand1 += operand2;
+                    break;
+
+            }
+
+        }
+        result.setText(operand1.toString());
+        newNumber.setText("");
     }
 }
